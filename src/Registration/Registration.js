5679e44770e6f371dc27d697a4bbde66b25eb332
PK <15170689+pawanline@users.noreply.github.com>
2025-03-03 18:45:39 +0530

    Bill generate code done

--- src/Registration/Registration.js
+++ src/Registration/Registration.js
@@ -373,14 +373,14 @@ const Registration = () => {
               <div class="card text-black" style={{ borderRadius: "25px" }}>
                 <div class="card-body p-md-5">
                   <div class="row">
-                    <div class="col-md-7 col-lg-6 col-xl-5 order-2 order-lg-1">
+                    <div class="col-md-2 col-lg-6 col-xl-5 order-2 order-lg-1">
                       <p class="text-center h1 fw-bold mb-5 mt-4">Siya Collection Bill Dashboard</p>
                       <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                      <img
+                      {/* <img
                         src={vidhataImage} // Use the imported image
                         class="img-fluid"
                         alt="Shel Digtal Library"
-                      />
+                      /> */}
                     </div>
                       <form  class="col-md-7" onSubmit={handleSubmit}>
                       <div  className="row mt-3">
@@ -619,7 +619,9 @@ const Registration = () => {
                         {/* PDF Preview Section */}
     <div className="col-md-5 d-flex align-items-center justify-content-center">
       <div ref={pdfRef}  classNam="pdf-preview" style={{ width: "48mm", padding: "10px", background: "#fff" }}>
-        <h3>Bill Receipt</h3>
+        <h5>Siya Collection</h5>
+        <p>Mayur Vihar Phase 3</p>
+        <p><i>110096</i></p>
         <p><b>Product Code:</b> {values.productCode}</p>
         <p><b>Brand:</b> {values.productBrand}</p>
         <p><b>Category:</b> {values.productCategory}</p>
@@ -631,6 +633,7 @@ const Registration = () => {
         <p><b>Payment Mode:</b> {values.paymentMode}</p>
       </div>
     </div>
+    
                     </div>
                     
                   </div>
